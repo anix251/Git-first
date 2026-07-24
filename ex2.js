7c86481df49b8672e51602e5d77ed2fb89213482
anindolia251-droid <anindolia251@gmail.com>
2026-07-24 10:31:57 +0530

    filter map reduce

---
+++ ex2.js
@@ -0,0 +1,6 @@
+const a=[1,2,3,4,5,6,7,8,9,10];
+const even=a.filter((i)=>(i%2==0));
+console.log("a=",a);
+console.log("Even=",even);
+const square=a.map((i)=>(i*i));
+console.log("Square=",square);
